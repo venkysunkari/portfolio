f43faa5a6c39bbd3278ad33043282c02d1b8fd06
venkysunkari <68497867+venkysunkari@users.noreply.github.com>
2020-09-14 18:48:27 +0530

    Add files via upload

---
+++ port.js
@@ -0,0 +1,17 @@
+alert("Thanks for showing interest!!")
+function openMenu() {
+    
+    var open = document.querySelector("ul");
+    
+open.style.right="0%"
+
+}
+
+function closeMenu() {
+
+   var close = document.querySelector("ul");
+
+       
+   close.style.right="-1000%"
+
+}
